cddd6988a0a865e288529771ded9f5088d7d6363
Esther Carrasco <silviacarrascoescobar@gmail.com>
2021-01-20 11:34:10 -0400

    week 2 portal

--- web/common/footer.php
+++ web/common/footer.php
@@ -1,2 +1,2 @@
-<p>&copy;Esther Silvia Carrasco Escobar | <a href="http://www.byui.edu/online">BYU-Idaho Online Learning</a></p>
+<p>&copy;Esther Silvia Carrasco Escobar | <a href="http://www.byui.edu/online">BYU-Idaho Online Learning</a> | <?php date('F j, Y') ?></p>
     
